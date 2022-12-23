d63448ee1da4400a58ada40d5dcdeaa987e1f500
sanit7722 <sanit7722@gmail.com>
2022-12-23 20:43:52 +0530

    Added Rating to User Service

--- UserMicroservice/src/main/java/com/hotelrating/userservice/entities/User.java
+++ UserMicroservice/src/main/java/com/hotelrating/userservice/entities/User.java
@@ -1,8 +1,12 @@
 package com.hotelrating.userservice.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,5 +28,8 @@ public class User {
 	private String name;
 	private String email;
 	private String about;
+	
+	@Transient
+	private List<Rating> ratings=new ArrayList<>();
 
 }
