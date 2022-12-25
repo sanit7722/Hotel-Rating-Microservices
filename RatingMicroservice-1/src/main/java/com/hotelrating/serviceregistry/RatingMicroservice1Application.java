d39d0130d63864d59b4160101692e5d1b1981592
sanit7722 <sanit7722@gmail.com>
2022-12-25 19:35:58 +0530

    Added Service Registry

---
+++ RatingMicroservice-1/src/main/java/com/hotelrating/serviceregistry/RatingMicroservice1Application.java
@@ -0,0 +1,13 @@
+package com.hotelrating.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingMicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingMicroservice1Application.class, args);
+	}
+
+}
