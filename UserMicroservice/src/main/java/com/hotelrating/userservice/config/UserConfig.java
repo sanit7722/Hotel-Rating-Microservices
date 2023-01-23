c253567626966cd976f53d932f7904b8e0fe9278
sanit7722 <sanit7722@gmail.com>
2023-01-23 21:03:13 +0530

    Implemented FeignClient Interceptor

--- UserMicroservice/src/main/java/com/hotelrating/userservice/config/UserConfig.java
+++ UserMicroservice/src/main/java/com/hotelrating/userservice/config/UserConfig.java
@@ -3,6 +3,12 @@ package com.hotelrating.userservice.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,5 +18,15 @@ public class UserConfig {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    
+    @Bean
+    public OAuth2AuthorizedClientManager manager(ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository) {
+    	
+    	
+    	OAuth2AuthorizedClientProvider provider=OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
+    	DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientRepository);
+    	defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
+    	return defaultOAuth2AuthorizedClientManager;
     }
 }
